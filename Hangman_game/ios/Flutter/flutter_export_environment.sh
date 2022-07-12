fe565d47a5dffd5051988adf5a44616454984f4f
Prashant Nayak <41485907+urperfectdude@users.noreply.github.com>
2022-07-12 22:28:35 +0530

    Add files via upload

---
+++ Hangman_game/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Prashant\OneDrive\Documents\Flutter\stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Prashant\Downloads\Flutter Projects\Hangman-Game-Flutter-master\Hangman-Game-Flutter-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.2.0"
+export "FLUTTER_BUILD_NUMBER=4005"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
